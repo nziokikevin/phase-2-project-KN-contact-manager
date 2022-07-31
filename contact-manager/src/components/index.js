0d4f51917064c0b703bb95fef274039ae1a2de0a
nziokikevin <kevinnzioki10@gmail.com>
2022-07-31 10:11:42 +0300

    recent

--- contact-manager/src/components/index.js
+++ contact-manager/src/components/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./index.css"
 
 
